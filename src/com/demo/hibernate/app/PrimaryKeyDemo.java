384fb842170c640acd8d94557cb1672a8c3b9bc6
mmizani <maryam.mizani@gmail.com>
2021-08-16 07:01:27 -0400

    Add primary key generation strategy

--- src/com/demo/hibernate/app/PrimaryKeyDemo.java
+++ src/com/demo/hibernate/app/PrimaryKeyDemo.java
@@ -6,10 +6,9 @@ import org.hibernate.cfg.Configuration;
 
 import com.demo.hibernate.entity.Student;
 
-public class CreateStudentDemo {
+public class PrimaryKeyDemo {
 
 	public static void main(String[] args) {
-		
 		// Create Session Factory
 		SessionFactory lFactory = new Configuration()
 				.configure("hibernate.cfg.xml")
@@ -19,16 +18,20 @@ public class CreateStudentDemo {
 		// Create a Session
 		Session lSession = lFactory.getCurrentSession();
 		try {		
-			// Create a student Object
-			System.out.println("Creating new student object");
-			Student lStudent = new Student("Paul", "wall", "paul@demo.com");
+			// Create 3 student Objects
+			System.out.println("Create 3 student objects");
+			Student lStudent1 = new Student("J", "Doe", "j@demo.com");
+			Student lStudent2 = new Student("Mary", "p", "mary@demo.com");
+			Student lStudent3 = new Student("Bonita", "q", "bonita@demo.com");
 			
 			// Start a transaction
 			lSession.beginTransaction();
 			
 			// Save the student Object
 			System.out.println("Saving the student");
-			lSession.save(lStudent);
+			lSession.save(lStudent1);
+			lSession.save(lStudent2);
+			lSession.save(lStudent3);
 			
 			// Commit transaction
 			lSession.getTransaction().commit();
