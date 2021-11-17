6f5277be1a390855cfbfa0a53082104b2daa45ef
mmizani <maryam.mizani@gmail.com>
2021-11-17 09:04:41 -0500

    OneToOne Uni-dir mapping: Delete Instructor and instructorDetail

--- src/com/demo/hibernate/entity/Instructor.java
+++ src/com/demo/hibernate/entity/Instructor.java
@@ -40,6 +40,10 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
+	public Instructor() {
+		
+	}
+	
 	public Instructor(String firstName, String lastName, String email) {
 		super();
 		this.firstName = firstName;
---
+++ src/com/demo/hibernate/oneToOneUniApp/DeleteOneToOneUniDemo.java
@@ -0,0 +1,48 @@
+package com.demo.hibernate.oneToOneUniApp;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.demo.hibernate.entity.Instructor;
+import com.demo.hibernate.entity.InstructorDetail;
+import com.demo.hibernate.entity.Student;
+
+public class DeleteOneToOneUniDemo {
+
+	public static void main(String[] args) {
+		
+		// Create Session Factory
+		SessionFactory lFactory = new Configuration()
+				.configure("hibernate.oneToOneUniCfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+						
+		// Create a Session
+		Session lSession = lFactory.getCurrentSession();
+		try {									
+			
+			// Start a transaction
+			lSession.beginTransaction();
+			
+			// get Instructor by primary key / id
+			int lId = 1;
+			Instructor lInstructor = lSession.get(Instructor.class, lId);
+			
+			// Delete the instructor
+			System.out.println("Found Instructor: " + lInstructor);
+			if(lInstructor != null) {
+				// This will also delete InstructorDetail because of CascadeType.ALL
+				lSession.delete(lInstructor);
+			}
+						
+			// Commit transaction
+			lSession.getTransaction().commit();
+			System.out.println("Done");
+		}
+		finally {
+			lFactory.close();
+		}
+	}
+}
