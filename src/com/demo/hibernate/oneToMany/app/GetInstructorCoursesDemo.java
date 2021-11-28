ed7997d66505e2ffec8f060f0ab9260f113e766f
mmizani <maryam.mizani@gmail.com>
2021-11-27 19:53:16 -0500

    OneToMany: Get an Instructor and the associated courses

--- src/com/demo/hibernate/oneToMany/app/GetInstructorCoursesDemo.java
+++ src/com/demo/hibernate/oneToMany/app/GetInstructorCoursesDemo.java
@@ -8,7 +8,7 @@ import com.demo.hibernate.oneToMany.entity.Course;
 import com.demo.hibernate.oneToMany.entity.Instructor;
 import com.demo.hibernate.oneToMany.entity.InstructorDetail;
 
-public class CreateCoursesDemo {
+public class GetInstructorCoursesDemo {
 
 	public static void main(String[] args) {
 		
@@ -29,18 +29,10 @@ public class CreateCoursesDemo {
 			// Get Instructor from DB
 			int lId = 1;
 			Instructor lInstructor = lSession.get(Instructor.class, lId);
+			System.out.println("Instructor: " + lInstructor);
 			
-			// Create Some Courses
-			Course lCourse1 = new Course("Guitar");
-			Course lCourse2 = new Course("Pinball");
-			
-			// Add Courses to the Instructor
-			lInstructor.add(lCourse1);
-			lInstructor.add(lCourse2);
-
-			//Save the Courses
-			lSession.save(lCourse1);
-			lSession.save(lCourse2);
+			// get courses for the Instructor
+			System.out.println("Courses: " + lInstructor.getCourses());
 			
 			// Commit transaction
 			lSession.getTransaction().commit();
