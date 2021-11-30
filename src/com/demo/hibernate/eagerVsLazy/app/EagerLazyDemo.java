ee419bc03466cce35feea204612ad9f9ee49c1f6
mmizani <maryam.mizani@gmail.com>
2021-11-30 06:06:55 -0500

    Eager-Vs-Lazy loading: Fetch all the course info of an instructor when fetching the instructor

---
+++ src/com/demo/hibernate/eagerVsLazy/app/EagerLazyDemo.java
@@ -0,0 +1,50 @@
+package com.demo.hibernate.eagerVsLazy.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.demo.hibernate.oneToMany.entity.Course;
+import com.demo.hibernate.oneToMany.entity.Instructor;
+import com.demo.hibernate.oneToMany.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) {
+		
+		// Create Session Factory
+		SessionFactory lFactory = new Configuration()
+				.configure("hibernate.oneToManyCfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+						
+		// Create a Session
+		Session lSession = lFactory.getCurrentSession();
+		try {									
+			// Start a transaction
+			lSession.beginTransaction();
+			
+			// Get Instructor from DB
+			int lId = 1;
+			Instructor lInstructor = lSession.get(Instructor.class, lId);
+			System.out.println("Instructor: " + lInstructor);  // This will print all the Course info of this instructor as well
+			
+			// get courses for the Instructor
+			System.out.println("Courses: " + lInstructor.getCourses());
+			
+			// Commit transaction
+			lSession.getTransaction().commit();
+			System.out.println("Done");
+		}
+		catch(Exception exc) {				
+			exc.printStackTrace();
+		}
+		finally {
+			// handle connection leak issue
+			lSession.close();
+			lFactory.close();
+		}
+	}
+}
